663b05f545e2aba6e4bb41e0982614746c852ca1
István Davorin Ács <mr.istvan.acs@gmail.com>
2024-01-10 21:37:47 +0100

    Quick typo fix

--- lib/trace/geometry/plane.cc
+++ lib/trace/geometry/plane.cc
@@ -30,7 +30,7 @@ auto Plane::Collide(Ray const& ray) -> std::optional<Collision>
     // take its dot product with u_, which is a vector representing the "side" of the plane.
     // This dot product as all dot products represents projecting P onto u_ and multiplying this
     // vectors length by the length of u_. So if we divide the result by u_'s length, we get the length
-    // of the projection. Then it is easy to check if we are withing the rectangle as the valid range is
+    // of the projection. Then it is easy to check if we are within the rectangle as the valid range is
     // [0, |u_|]. Same is true with v_.
     auto alpha = dot(u_, collisionPoint - Q_) / u_.Length();
     if (0.0 > alpha || alpha > u_.Length()) { return std::optional<Collision>{}; }
